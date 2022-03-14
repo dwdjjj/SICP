0cbd3e08c73c4ec0fa44eb4336e29eecc0ebb419
dwdjjj <dwdjjj@naver.com>
2022-03-14 20:42:32 +0900

    1.19 update

--- CH1/1.19.js
+++ CH1/1.19.js
@@ -6,12 +6,17 @@ function fib(n) {
   return fib_iter(1, 0, 0, 1, n);
 }
 
-function fib_iter(a, b, p, q, c) {
-  return c === 0
+function fib_iter(a, b, p, q, count) {
+  return count === 0
     ? b
-    : is_even(c)
-    ? fib_iter(a, b, p * p + q * q, 2 * p * q + q * q, c / 2)
-    : fib_iter(b * q + a * q + a * p, b * p + a * q, p, q, c - 1);
+    : is_even(count)
+    ? fib_iter(a, b, p * p + q * q, 2 * p * q + q * q, count / 2)
+    : fib_iter(b * q + a * q + a * p, b * p + a * q, p, q, count - 1);
 }
 
-fib(5);
+console.log(fib(10));
+
+// |a b| |p+q q|
+//       |q   p|
+
+// T2 : 행렬식 한번더 연산한 것
